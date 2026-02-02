4498cb70247e89ad0f8c9e9e6cd3773a782ef631
samy <samysamyromioromio@gmail.com>
2026-02-02 19:40:58 +0200

    Create delete user endpoint

---
+++ Tradify/Tradify.Core/Features/User/Commands/Models/DeleteUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Core.Bases;
+
+namespace Tradify.Core.Features.User.Commands.Models
+{
+    public class DeleteUserCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+        public DeleteUserCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
