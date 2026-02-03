4e7e44df2798a65f94ead23a241675b465569e03
salspil <salsabilami598@gmail.com>
2026-02-03 04:57:56 +0200

    Add Product endpoint

---
+++ Tradify/Tradify.Core/Features/Product/Commands/Models/AddProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Core.Bases;
+
+namespace Tradify.Core.Features.Product.Commands.Models
+{
+    public class AddProductCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public int CategoryId { get; set; }
+        public int StoreId { get; set; }
+        public int NumberOfProductInStock { get; set; }
+     
+    }
+}
---
+++ Tradify/Tradify.Core/Mapping/ProductMapping/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tradify.Core.Mapping.ProductMapping
+{
+    public partial class ProductProfile : Profile
+    {
+        public ProductProfile() 
+        {
+            AddProductMapping();
+        }
+    }
+}
