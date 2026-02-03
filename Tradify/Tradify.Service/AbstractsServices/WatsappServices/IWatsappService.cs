d44a8dcbbf11bec2eba7434bf9874d5144195773
samy <samysamyromioromio@gmail.com>
2026-02-03 20:16:19 +0200

    Add Watsapp Service

---
+++ Tradify/Tradify.Service/AbstractsServices/WatsappServices/IWatsappService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tradify.Service.AbstractsServices.WatsappServices
+{
+    public interface IWatsappService
+    {
+        Task<bool> SendVerificationCodeAsync(string phoneNumber, string code);
+    }
+}
---
+++ Tradify/Tradify.Service/Services/WatsappServices/WatsappService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Data.Helpers;
+using Tradify.Service.AbstractsServices.IdentityServices;
+using Tradify.Service.AbstractsServices.WatsappServices;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Tradify.Service.Services.WatsappServices
+{
+    public class WatsappService : IWatsappService
+    {
+        private readonly TwilioSettings twilioSettings;
+        private readonly IUserService userService;
+        public WatsappService( TwilioSettings twilioSettings , IUserService userService)
+        {
+            this.userService=userService;
+             this.twilioSettings = twilioSettings;
+            Twilio.TwilioClient.Init(twilioSettings.AccountSID, twilioSettings.AuthToken);
+        }
+        public async Task<bool> SendVerificationCodeAsync(string phoneNumber, string code)
+        {
+
+            try
+            {
+                var isnumber = userService.IsPhone(phoneNumber);
+                if (!isnumber) return false;
+                var message = await MessageResource.CreateAsync(
+                   from: new PhoneNumber($"whatsapp:{twilioSettings.WhatsAppFrom}"),
+                to: new PhoneNumber($"whatsapp:{phoneNumber}"),
+                body: $"Your verification code is: {code}"
+                    )  ;
+
+                  return !string.IsNullOrEmpty(message.Sid);
+
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+    }
+}
