48291988391f0df55d6c43220b8edde08abe8929
samy <samysamyromioromio@gmail.com>
2026-04-19 14:53:56 +0200

    update on UserService

---
+++ Tradify/Tradify.Core/Features/User/Queries/Validations/GetUserByTokenQueryValidator.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Core.Features.Seller.Command.Models;
+using Tradify.Core.Features.User.Commands.Models;
+using Tradify.Core.Features.User.Queries.Models;
+using Tradify.Core.Resources.Service;
+
+namespace Tradify.Core.Features.Seller.Command.Validations
+{
+    public class GetUserByTokenQueryValidator : AbstractValidator<GetUserByTokenQuery>
+    {
+        #region Fields 
+
+        private readonly LocalizationService localize;
+        #endregion
+
+        #region constructor
+        public GetUserByTokenQueryValidator(LocalizationService localization)
+        {
+            this.localize = localization;
+            ApplyValidations();
+        }
+
+
+        #endregion
+
+        #region Validations
+
+
+        public void ApplyValidations()
+        {
+
+
+
+
+            RuleFor(x => x.AccessToken)
+               .NotEmpty().WithMessage(localize.Get("NotEmpty"))
+             .NotNull().WithMessage(localize.Get("Required"));
+             
+
+           
+
+
+        }
+
+
+        #endregion
+
+
+    }
+}
